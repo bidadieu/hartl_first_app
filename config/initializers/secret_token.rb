5dac899ddc2f50b0c413df2b14e1c5f983e4f739
Bid Adieu <todd@bidadi.eu>
2010-11-17 20:11:33 -0600

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HartlFirstApp::Application.config.secret_token = '714ff3c7d3b6467e40229f9d5544c2502a9c4c55cb6058b282acc031e8925caf4b14203a19202b8db0e6ba5ffe4a7011fc2a3b668324db39206f548366b30916'
